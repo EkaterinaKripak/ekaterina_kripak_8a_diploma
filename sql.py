283136cec8faeee718fcdbb021b60adf47387e22
EkaterinaKripak <143655425+EkaterinaKripak@users.noreply.github.com>
2023-10-22 00:35:39 +0300

    Create sql.py

---
+++ sql.py
@@ -0,0 +1,17 @@
+#Задание №1
+SELECT c.login,
+       COUNT(o."courierId")
+FROM "Couriers" AS c LEFT JOIN "Orders" AS o ON c.id=o."courierId"
+WHERE o."InDelivery"=true
+GROUP BY c.login;
+
+#Задание №2
+
+SELECT track,
+    CASE
+        WHEN finished = true THEN 2
+        WHEN cancelled = true THEN -1
+        WHEN InDelivery = true THEN 1
+        ELSE 0
+    END AS status
+FROM "Orders";
