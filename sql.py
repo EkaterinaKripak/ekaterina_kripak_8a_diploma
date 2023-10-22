56053deb83335588857cc7b19ddccb3d859d948c
EkaterinaKripak <143655425+EkaterinaKripak@users.noreply.github.com>
2023-10-22 20:01:16 +0300

    Update sql.py

--- sql.py
+++ sql.py
@@ -11,7 +11,7 @@ SELECT track,
     CASE
         WHEN finished = true THEN 2
         WHEN cancelled = true THEN -1
-        WHEN InDelivery = true THEN 1
+        WHEN "InDelivery" = true THEN 1
         ELSE 0
     END AS status
 FROM "Orders";
